8dc73561bd4cde010f823d20b0d2cc2c52f11571
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-07-15 11:30:08 +0530

    Redis Lite Header file defined

---
+++ include/redisLite.h
@@ -0,0 +1,29 @@
+#ifndef REDISCLI_H
+#define REDISCLI_H
+
+#include <string>
+#include "HashMap.h"
+
+class RedisCLI
+{
+public:
+    
+    void run();
+
+private:
+    
+    HashMap<std::string, std::string> store;
+
+    void handleSet(const std::string& key, const std::string& value);
+    void handleGet(const std::string& key);
+    void handleDel(const std::string& key);
+    void handleExists(const std::string& key);
+    void handleCount();
+    void handleClear();
+
+    
+    void printHelp() const;
+};
+
+#include "redisLite.cpp"
+#endif
